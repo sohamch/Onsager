501b9c597ea99a273037f123b70952ca86c90ab8
sohamch <ch.soham@gmail.com>
2023-02-17 21:12:36 -0600

    Removing flipInd. Should have been a long time ago.

--- onsager/DB_structs.py
+++ onsager/DB_structs.py
@@ -38,11 +38,12 @@ class dumbbell(namedtuple('dumbbell', 'iorind R')):
         if not i_new == container.iorlist[gdumb.indexmap[0][self.iorind]][0]:
             raise ValueError("Gdumb and G not consistent")
         newind = gdumb.indexmap[0][self.iorind]
-        if pure:
-            flipind = container.gflip(gdumb, self.iorind)
-            return self.__class__(newind, R_new), flipind
-        else:
-            return self.__class__(newind, R_new)
+        return self.__class__(newind, R_new)
+        # if pure:
+        #     flipind = container.gflip(gdumb, self.iorind)
+        #     return self.__class__(newind, R_new), flipind
+        # else:
+        #     return self.__class__(newind, R_new)
 
     def __add__(self, other):
         if not isinstance(other, np.ndarray):
